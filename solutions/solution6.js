bb22f07bb75ec16cfe0f35765d6b79254b9443c8
LincKim <lincolnkimutai@gmail.com>
2023-07-15 08:29:13 +0300

    Solution six done

---
+++ solutions/solution6.js
@@ -0,0 +1,17 @@
+// Write a program that prints a multiplication table for numbers up to 12.
+
+function multiplicationTable() {
+    let table = ''
+    
+    for (let i = 1; i <= 12; i++) {  // The outer loop iterates from 1-12, representing the rows
+      for (let j = 1; j <= 12; j++) {  // iterates from 1-12 representing the columns
+        table += (i * j) + '\t'  // the '\t'(tab character) separates the new entry on the table.
+      }
+      table += '\n'  // the '\n' (newline character) move items to new line
+    }
+  
+    console.log(table)
+  }
+  
+  multiplicationTable();
+  
