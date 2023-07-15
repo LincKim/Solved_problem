6d300ef1d984dbbdd3d1b747fa6c50200e019de7
LincKim <lincolnkimutai@gmail.com>
2023-07-15 09:38:58 +0300

    Done

--- solutions/solution8.js
+++ solutions/solution8.js
@@ -1,2 +1,40 @@
 // Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
 
+const prompt = require('prompt-sync')({sigit: true})
+
+function guessingGame() {
+    const secretNumber = Math.floor(Math.random() * 100) + 1;
+    let previousGuess = null;  // keeps track of the previous guess to handle consecutive identical guesses
+    let tries = 0;  // variable counts the number of tries.
+
+  
+    while (true) {
+      let guess = Number.parseInt(prompt('Guess a number between 1 and 100: '));
+      
+      if (isNaN(guess)) {
+        console.log('Invalid input. Please enter a valid number.');
+        continue;
+      }
+  
+      if (guess === previousGuess) {
+        console.log('You just guessed that number. Try a different one.');
+        continue;
+      }
+  
+      tries++; // For each valid guess, the program increments the tries counter and compares the guess with the secret number
+  
+      if (guess < secretNumber) {
+        console.log('Too small! Try a higher number.');
+      } else if (guess > secretNumber) {
+        console.log('Too large! Try a lower number.');
+      } else {
+        console.log(`Congratulations! You guessed the secret number ${secretNumber} in ${tries} tries.`);
+        break;
+      }
+  
+      previousGuess = guess;
+    }
+  }
+  
+  guessingGame();
+  
--- solutions/solution9.js
+++ solutions/solution9.js
@@ -1,1 +1,21 @@
 // Write a program that prints the next 20 leap years.
+
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  } // The function uses the leap year rule: a year is a leap year if it is divisible by 4 but not divisible by 100, except if it is also divisible by 400.
+  
+  function printNext20LeapYears() {
+    let currentYear = new Date().getFullYear();  // this gets the current yeat we are in at the moment
+    let count = 0;
+  
+    while (count < 20) {
+      if (isLeapYear(currentYear)) {
+        console.log(currentYear);
+        count++;
+      }
+      currentYear++;
+    }
+  }
+  
+  printNext20LeapYears();
+  
